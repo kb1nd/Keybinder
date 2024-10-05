049da4e220668ed33a05858e5a0b908c132588db
Keybinder <cutygg06@gmail.com>
2024-10-05 16:14:38 +0200

    fix 2

--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,41 @@
 const std = @import("std");
 const network = @import("network");
-const x11 = @cImport({
-    @cInclude("Xlib.h");
-});
+const x11 = @cImport(@cInclude("Xlib.h"));
 const dvui = @import("dvui");
 const sdl = @import("sdl");
 var allocator = std.heap.GeneralPurposeAllocator(.{}){};
-const active = switch(Cache.payload) {
-    1 => true,
-    0 => false,
-};
 pub fn main() !void {
     const gpa = allocator.allocator();
     const file = try std.fs.cwd().openFile("cache.json", .{});
     defer file.close();
     const data = try file.readToEndAlloc(gpa, std.math.maxInt(usize));
     defer gpa.free(data);
-    const json = try std.json.parseFromSlice(struct {toggleKey: u8, repeatDelay: i8,  bindedKey: u8, port: i8}, gpa, data, .{});
-    defer json.deinit(gpa);
-    const cache = json.value;
+    const parse = try std.json.parseFromSlice(struct {
+        toggleKey: u8,
+        repeatDelay: u16,
+        bindedKey: u8,
+        port: u16,
+    }, gpa, data, .{});
+    defer parse.deinit();
+    const Cache = parse.value;
     const display = x11.XOpenDisplay(null);
     const event = x11.XEvent;
-    const toggle = struct {
-        const counter = 0;
-        const status = switch(toggle.counter) {
-            1 => {
-                true;
-            },
-            2 => {
-                false;
-                counter -= 2;
-            },
-            else => {
-                false;
-            },
-        }
-    }
-    const keycode = x11.XKeysymToKeycode(display, x11.XStringToKeysym(bindedKey));
     const window = x11.XDefaultRootWindow(display);
-    const key_held = if(event.keycode == keycode and active == true and toggle.status == false) {
-        if(std.mem.eql(u8, event.type, "KeyPress")) {
-            true;
-        } else if(std.mem.eql(u8, event.type, "KeyRelease")) {
-            false;
-        }
-    } else {
-        false;
+    const Sequence = struct {
+        const keycode = undefined;
+        const @"type" = undefined;
+        const toggle = struct {
+            var counter: u16 = 0;
+            const status = switch(counter) {
+                1 => true,
+                2 => {
+                    false;
+                    counter -= 2;
+                },
+                else => false,
+            };
+        };
+        const status = if(keycode == x11.XKeysymToKeycode(display, x11.XStringToKeysym(Cache.bindedKey)) and Socket.toggle == true and toggle.status == false and std.mem.eql(u8, @"type", "KeyPress")) true || false;
     };
     try network.init();
     defer network.deinit();
@@ -55,20 +45,12 @@ pub fn main() !void {
         .address = .{
             .ipv4 = network.Address.IPv4.any
         },
-        .port = cache.port,
+        .port = Cache.port,
     });
     try server.listen();
     std.log.info("listening at {}\n", .{
-        try server.getLocalEndPoint()
+        try server.getLocalEndPoint(),
     });
-    while (true) {
-        std.debug.print("Waiting for connection\n", .{});
-        const client = try gpa.create(Client);
-        client.* = Client{
-            .conn = try server.accept(),
-            .handle_frame = async client.handle(),
-        };
-    }
     var backend = try sdl.initWindow(.{
         .allocator = gpa,
         .size = .{
@@ -84,36 +66,48 @@ pub fn main() !void {
         .icon = @embedFile("appIcon.png"),
     });
     defer backend.deinit();
-    var win = try dvui.Window.init(@src(), gpa, backend.backend(), .{});
-    defer win.deinit();
+    var gui = try dvui.Window.init(@src(), gpa, backend.backend(), .{});
+    defer gui.deinit();
     x11.XSelectInput(display, window, x11.KeyPressMask | x11.KeyReleaseMask);
-    x11.XMapWindow(display, window);
-    x11.XNextEvent(display, &event);
-    while (key_held == true) {
-        event.keycode == keycode;
-        x11.XSendEvent(display, window, 1, x11.KeyPressMask, &event);
-        x11.XFlush(display);
-    }
-    main_loop: while (true) {
-        if(event.keycode == x11.XKeysymToKeycode(display, x11.XStringToKeysym(cache.toggleKey))) {
-            toggle.counter += 1;
+    main_loop: while(true) {
+        x11.XNextEvent(display, &event);
+        if(std.mem.eql(u8, event.type, "KeyPress")) {
+            if(std.mem.eql(u8, event.type, x11.XKeysymToKeycode(display, x11.XStringToKeysym(Cache.toggleKey)))) Sequence.toggle.counter += 1;
+            Sequence.keycode = event.keycode;
+            Sequence.type = event.type;
+        } else if(std.mem.eql(u8, event.type, "KeyRelease")) {
+            Sequence.keycode = event.keycode;
+            Sequence.type = event.type;
         }
-        const nstime = win.beginWait(backend.hasEvent());
-        try win.begin(nstime);
-        const quit = try backend.addAllEvents(&win);
+        switch(Sequence.status) {
+            true => {
+                event.keycode == x11.XKeysymToKeycode(display, x11.XStringToKeysym(Cache.bindedKey));
+                x11.XSendEvent(display, window, 1, x11.KeyPressMask, &event);
+                x11.XFlush(display);
+            },
+            else => {},
+        }
+        const client = try gpa.create(Client);
+        client.* = Client{
+            .conn = try server.accept(),
+            .handle_frame = async client.handle(),
+        };
+        const nstime = gui.beginWait(backend.hasEvent());
+        try gui.begin(nstime);
+        const quit = try backend.addAllEvents(&gui);
         if (quit) break :main_loop;
         _ = sdl.c.SDL_SetRenderDrawColor(backend.renderer, 0, 0, 0, 255);
         _ = sdl.c.SDL_RenderClear(backend.renderer);
-        try gui_frame();
-        const end_micros = try win.end(.{});
-        backend.setCursor(win.cursorRequested());
+        try frame();
+        const end = try gui.end(.{});
+        backend.setCursor(gui.cursorRequested());
         backend.renderPresent();
-        const wait_event_micros = win.waitTime(end_micros, null);
-        backend.waitEventTimeout(wait_event_micros);
+        const wait = gui.waitTime(end, null);
+        backend.waitEventTimeout(wait);
     }
     x11.XCloseDisplay(display);
 }
-fn gui_frame() !void {
+fn frame() !void {
     {
         var scroll = try dvui.scrollArea(@src(), .{}, .{
             .expand = .both,
@@ -148,15 +142,17 @@ const Client = struct {
     allocator: allocator,
     fn handle(self: *Client, gpa: Client.allocator) !void {
         allocator = gpa.allocator();
-        while (true) {
+        while(true) {
             var buf: [100]u8 = undefined;
             const amt = try self.conn.receive(&buf);
             if (amt == 0) break;
-            Cache.payload = buf[0..amt];
-            std.debug.print("Received data from client: {s}", .{buf[0..amt]});
+            Socket.toggle = buf[0..amt];
+            std.log.info("Received data from client: {s}", .{
+                buf[0..amt],
+            });
         }
     }
 };
-const Cache = struct {
-    payload: i8 = 0,
+const Socket = struct {
+    var toggle: bool = false;
 };
