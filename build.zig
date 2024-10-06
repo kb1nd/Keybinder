bd1de3e4f519c04e992f4b6e9ee15e6e17280983
Keybinder <cutygg06@gmail.com>
2024-10-06 18:28:28 +0200

    fix 3

--- build.zig
+++ build.zig
@@ -1,4 +1,11 @@
 const std = @import("std");
+const versions = [_][]const u8{
+    "v1/X11",
+    "v1/XCB",
+    "v1/Wayland",
+    "v1/Windows",
+    "v1/Carbon-Cocoa",
+};
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -11,18 +18,28 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize
     });
-    const exe = b.addExecutable(.{
-        .target = target,
-        .optimize = optimize,
-        .name = "Keybinder's Keybinder",
-        .root_source_file = b.path("src/main.zig"),
-    });
-    exe.root_module.addImport("dvui", dvui_dep.module("dvui"));
-    exe.root_module.addImport("sdl", dvui_dep.module("SDLBackend"));
-    exe.root_module.addImport("network", network_dep.module("network"));
-    exe.addCSourceFile(.{
-        .file = b.path("X11/Xlib.h"),
-    });
-    exe.addIncludePath(b.path("X11/"));
-    b.installArtifact(exe);
+    inline for (versions) |ver| {
+        const exe = b.addExecutable(.{
+            .target = target,
+            .optimize = optimize,
+            .name = "Keybinder's " ++ ver ++ " Keybinder",
+            .root_source_file = b.path(ver ++ "/src/main.zig"),
+        });
+        exe.root_module.addImport("dvui", dvui_dep.module("dvui"));
+        exe.root_module.addImport("sdl", dvui_dep.module("SDLBackend"));
+        exe.root_module.addImport("network", network_dep.module("network"));
+        exe.addCSourceFile(.{
+            .file = b.path("X11/lib/Xlib.h"),
+        });
+
+        const compile_step = b.step("compile-" ++ ver, "Compile " ++ ver);
+        ompile_step.dependOn(&b.addInstallArtifact(exe, .{}).step);
+        b.getInstallStep().dependOn(compile_step);
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(compile_step);
+
+        const run_step = b.step(ver, "Run " ++ ver);
+        run_step.dependOn(&run_cmd.step);
+    }
 }
---
+++ v1/X11/src/main.zig
@@ -0,0 +1,159 @@
+const std = @import("std");
+const network = @import("network");
+const x11 = @cImport(@cInclude("Xlib.h"));
+const dvui = @import("dvui");
+const sdl = @import("sdl");
+var allocator = std.heap.GeneralPurposeAllocator(.{}){};
+pub fn main() !void {
+    const gpa = allocator.allocator();
+    const file = try std.fs.cwd().openFile("cache.json", .{});
+    defer file.close();
+    const data = try file.readToEndAlloc(gpa, std.math.maxInt(usize));
+    defer gpa.free(data);
+    const parse = try std.json.parseFromSlice(struct {
+        toggleKey: u8,
+        repeatDelay: u16,
+        bindedKey: u8,
+        port: u16,
+    }, gpa, data, .{});
+    defer parse.deinit();
+    const Cache = parse.value;
+    const display = x11.XOpenDisplay(null);
+    const event = x11.XEvent;
+    const window = x11.XDefaultRootWindow(display);
+    const masks = x11.KeyPressMask | x11.KeyReleaseMask;
+    const Sequence = struct {
+        const keycode = undefined;
+        const @"type" = undefined;
+        const toggle = struct {
+            var counter: u16 = 0;
+            const status = switch(counter) {
+                1 => true,
+                2 => {
+                    false;
+                    counter -= 2;
+                },
+                else => false,
+            };
+        };
+        const status = if(keycode == x11.XKeysymToKeycode(display, x11.XStringToKeysym(Cache.bindedKey)) and Socket.toggle == true and toggle.status == false and std.mem.eql(u8, @"type", "KeyPress")) true || false;
+    };
+    try network.init();
+    defer network.deinit();
+    var server = try network.Socket.create(.ipv4, .tcp);
+    defer server.close();
+    try server.bind(.{
+        .address = .{
+            .ipv4 = network.Address.IPv4.any
+        },
+        .port = Cache.port,
+    });
+    try server.listen();
+    std.log.info("listening at {}\n", .{
+        try server.getLocalEndPoint(),
+    });
+    var backend = try sdl.initWindow(.{
+        .allocator = gpa,
+        .size = .{
+            .w = 300.0,
+            .h = 400.0,
+        },
+        .min_size = .{
+            .w = 300.0,
+            .h = 400.0,
+        },
+        .vsync = true,
+        .title = "Keybinder's X11 Keybinder",
+        .icon = @embedFile("appIcon.png"),
+    });
+    defer backend.deinit();
+    var gui = try dvui.Window.init(@src(), gpa, backend.backend(), .{});
+    defer gui.deinit();
+    x11.XSelectInput(display, window, masks);
+    main_loop: while(true) {
+        x11.XNextEvent(display, &event);
+        if(std.mem.eql(u8, event.type, "KeyPress")) {
+            if(std.mem.eql(u8, event.type, x11.XKeysymToKeycode(display, x11.XStringToKeysym(Cache.toggleKey)))) Sequence.toggle.counter += 1;
+            Sequence.keycode = event.keycode;
+            Sequence.type = event.type;
+        } else if(std.mem.eql(u8, event.type, "KeyRelease")) {
+            Sequence.keycode = event.keycode;
+            Sequence.type = event.type;
+        }
+        switch(Sequence.status) {
+            true => {
+                event.keycode == x11.XKeysymToKeycode(display, x11.XStringToKeysym(Cache.bindedKey));
+                x11.XSendEvent(display, window, 1, x11.KeyPressMask, &event);
+                x11.XFlush(display);
+            },
+            else => {},
+        }
+        const client = try gpa.create(Client);
+        client.* = Client{
+            .conn = try server.accept(),
+            .handle_frame = async client.handle(),
+        };
+        const nstime = gui.beginWait(backend.hasEvent());
+        try gui.begin(nstime);
+        const quit = try backend.addAllEvents(&gui);
+        if (quit) break :main_loop;
+        _ = sdl.c.SDL_SetRenderDrawColor(backend.renderer, 0, 0, 0, 255);
+        _ = sdl.c.SDL_RenderClear(backend.renderer);
+        try frame();
+        const end = try gui.end(.{});
+        backend.setCursor(gui.cursorRequested());
+        backend.renderPresent();
+        const wait = gui.waitTime(end, null);
+        backend.waitEventTimeout(wait);
+    }
+    x11.XCloseDisplay(display);
+}
+fn frame() !void {
+    {
+        var scroll = try dvui.scrollArea(@src(), .{}, .{
+            .expand = .both,
+            .color_fill = .{
+                .name = .fill_window
+            },
+        });
+        defer scroll.deinit();
+        var header1 = try dvui.labelNoFmt(@src(), "Toggle Keybind", .{
+            .margin = .{
+                .x = 4
+            },
+        });
+        defer header1.deinit();
+        var box = try dvui.box(@src(), .horizontal, .{
+            .expand = .horizontal,
+            .min_size_content = .{
+                .h = 40
+            },
+            .background = true,
+            .margin = .{
+                .x = 8,
+                .w = 8
+            },
+        });
+        defer box.deinit();
+    }
+}
+const Client = struct {
+    conn: network.Socket,
+    handle_frame: @Frame(Client.handle),
+    allocator: allocator,
+    fn handle(self: *Client, gpa: Client.allocator) !void {
+        allocator = gpa.allocator();
+        while(true) {
+            var buf: [100]u8 = undefined;
+            const amt = try self.conn.receive(&buf);
+            if (amt == 0) break;
+            Socket.toggle = buf[0..amt];
+            std.log.info("Received data from client: {s}", .{
+                buf[0..amt],
+            });
+        }
+    }
+};
+const Socket = struct {
+    var toggle: bool = false;
+};
